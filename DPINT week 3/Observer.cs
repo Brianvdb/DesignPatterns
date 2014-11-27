c08f3107fd67fe5fc887a9cd517b1f9801ca421f
Sven Brettschneider <brettschneider.sven@gmail.com>
2014-11-27 12:10:04 +0100

    week 3 structuur aangemaakt

---
+++ DPINT week 3/Observer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_week_3
+{
+    public abstract class Observer
+    {
+        public virtual void Update(){
+
+        }
+    }
+}
---
+++ DPINT week 3/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_week_3
+{
+    public abstract class Subject
+    {
+        private List<Observer> observers;
+        public int MyProperty { get; set; }
+
+        public Subject(){
+            observers = new List<Observer>();
+        }
+        public void Notify()
+        {
+            observers.ForEach(ob => ob.Update());
+        }
+
+        public void Attach(Observer observer){
+            observers.Add(observer);
+        }
+
+        public void Detatch(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+    }
+}
