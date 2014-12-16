94f9d14da474fb22c2da4cf8874031fc234826d8
Brian <bcf.vandenbroek@student.avans.nl>
2014-12-16 13:29:09 +0100

    Sudoku web begin

---
+++ SudokuWeb_Week5/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using SudokuBasis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SudokuWeb_Week5.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+
+        public ActionResult Index()
+        {
+            SudokuGame game = Session["game"] as SudokuGame;
+            if (game == null)
+            {
+                NewGame();
+                game = Session["game"] as SudokuGame;
+            }
+
+            return View(game);
+        }
+
+        private void NewGame()
+        {
+            SudokuGame game = new SudokuGame();
+            game.NewGame();
+
+            Session["game"] = game;
+        }
+    }
+}
